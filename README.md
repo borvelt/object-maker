9a9488c928da8c4505d710bb5b95f64c6ef9dd56
borvelt <borvelt@gmail.com>
2018-10-06 11:56:02 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# object-maker
+
+_create object with string_
+
+*Write your own project README*
---
+++ webpack.config/dist.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'production',
+  entry: { index: __dirname + '/../src/index.js' },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+    chunkFilename: '[name].objectMaker.js',
+    publicPath: '/',
+    library: 'objectMaker',
+    libraryTarget: 'umd',
+  },
+  externals: [],
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: [
+            '@babel/plugin-transform-runtime',
+            '@babel/plugin-syntax-dynamic-import',
+          ],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist'], {
+      root: path.join(__dirname, '..'),
+    }),
+    new UglifyJsPlugin(),
+  ],
+  resolve: {
+    alias: {},
+  },
+}
